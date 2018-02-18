f90b966175197266c56907c49f34802cf73f8028
Max Jordan <max.jordan1306@gmail.com>
2018-02-18 18:23:14 +0000

    Added files. Website working with minor bugs..

---
+++ algorithm.js
@@ -0,0 +1,155 @@
+
+//file:///F:/Desktop/Code/index.html
+
+
+var startNode;
+var endNode;
+
+var openSet = [];
+var closedSet = [];
+
+var path;
+
+function removeFromArr(array,item){
+  for(var i=array.length-1;i>=0;i--){
+    if(array[i]==item){
+      array.splice(i,1)
+    }
+  }
+}
+
+function heuristic(a,b){
+  var d = Math.abs(a.i-b.i) + Math.abs(a.j-b.j);
+  return d;
+}
+
+function drawer(current){
+  ctx.clearRect(0,0,1500,750);
+  var temp = current
+  path = [];
+  path.push(temp)
+  while(temp.previous!=undefined){
+    path.push(temp.previous)
+    temp = temp.previous
+  }
+
+
+  for(var i=0;i<path.length;i++){
+    var a =path[i].i*25
+    var b =path[i].j*25
+    ctx.fillStyle="#5041f4"
+    ctx.fillRect(b,a,25,25);
+  }
+
+  ctx.fillStyle="#61bf44"
+  ctx.fillRect(startNode.j*25,startNode.i*25,25,25);
+  ctx.fillStyle="#af4646"
+  ctx.fillRect(endNode.j*25,endNode.i*25,25,25);
+
+  for(i=0;i<walls.length;i++){
+    ctx.fillStyle="#383838"
+    ctx.fillRect(walls[i].j*25,walls[i].i*25,25,25);
+  }
+}
+
+function astar(a){
+  var found=false;
+  openSet.push(startNode)
+  while(openSet.length>0){
+
+    var winner = 0;
+    for(var i=0; i<openSet.length;i++){
+      if (openSet[i].f < openSet[winner.f]){
+        winner=i
+      }
+    }
+
+
+    var current=openSet[winner]
+
+
+    if(current==endNode){
+      console.log("Done")
+      found=true
+      drawer(current);
+      break;
+    }
+
+    removeFromArr(openSet,current)
+    closedSet.push(current)
+    var neighbors = current.nb;
+    for (var i = 0; i < neighbors.length; i++) {
+      var neighbor = neighbors[i];
+      if (!closedSet.includes(neighbor) && neighbor.type!="wall") {
+        var tempG = current.g + heuristic(neighbor, current);
+
+        var newPath = false;
+
+        if (openSet.includes(neighbor)) {
+          if (tempG < neighbor.g) {
+            neighbor.g = tempG;
+            newPath = true;
+          }
+          } else {
+            neighbor.g = tempG;
+            newPath = true;
+            openSet.push(neighbor);
+          }
+
+          if (newPath) {
+            neighbor.h = heuristic(neighbor, endNode);
+            neighbor.f = neighbor.g + neighbor.h;
+            neighbor.previous = current;
+          }
+      }
+
+    }
+  }
+
+  if(!found){
+    console.log("No Soloutions")
+  }
+
+  openSet = [];
+  closedSet = [];
+  path = undefined;
+  startNode=undefined;
+  endNode=undefined;
+
+  walls=[];
+
+  for(var i = 0; i<rows; i++){
+    for(var j = 0; j<cols; j++){
+      board[i][j].previous = undefined;
+      board[i][j].f = 0;
+      board[i][j].g = 0;
+      board[i][j].h = 0;
+    }
+  }
+  //if we get here its over
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app.js
@@ -0,0 +1,261 @@
+
+var c = document.getElementById('gameCanvas');
+var ctx = c.getContext('2d');
+
+var mouseX;
+var mouseY;
+
+var mouseDown=false
+
+var type="wall"
+
+var startPoints=0
+var endPoints=0
+
+
+var rows=30
+var cols=60
+
+var wallCount = 0
+var walls=[];
+
+
+function Node(i,j){
+  this.i=i;
+  this.j=j;
+  this.type="empty";
+  this.f=0;
+  this.h=0;
+  this.g=0;
+  this.start=false;
+  this.end=false;
+  this.nb = [];
+  this.previous = undefined;
+  this.show = function(col){
+    ctx.fillStyle=col
+    ctx.fillRect(j*25,i*25,25,25);
+  }
+
+  this.addNb = function(){
+    var x = this.j
+    var y = this.i
+    if(x<cols-1){
+      this.nb.push(board[y][x+1])
+    }
+    if(x>0){
+      this.nb.push(board[y][x-1])
+
+    }
+    if(y<rows-1){
+      this.nb.push(board[y+1][x])
+    }
+    if(y>0){
+      this.nb.push(board[y-1][x])
+    }
+
+
+  }
+}
+
+
+//60x30 grid
+/***
+0=empty Space
+1=Wall
+2=Start
+3=End
+***/
+
+
+var board = new Array(cols)
+for (var i = 0; i < rows; i++) {
+  board[i] = new Array(cols);
+}
+console.log(board)
+
+
+for(var i = 0; i<rows; i++){
+  for(var j = 0; j<cols; j++){
+    board[i][j]= new Node(i,j);
+  }
+}
+
+for(var i = 0; i<rows; i++){
+  for(var j = 0; j<cols; j++){
+    board[i][j].addNb();
+  }
+}
+
+
+c.addEventListener("mousemove", checkPos);
+c.addEventListener("mouseup", checkClick);
+c.addEventListener("mousedown", downHandler);
+
+function downHandler(){
+  mouseDown=true
+  draw()
+}
+
+function updateText(){
+  document.getElementById("button2").innerHTML="End Point <span id='red'>("+endPoints+"/1)</span>"
+  document.getElementById("button1").innerHTML="Start Point <span id='red'>("+startPoints+"/1)</span>"
+  document.getElementById("button3").innerHTML="Wall <span id='red'>(" +wallCount+")</span>"
+}
+
+
+function draw(){
+  xDraw = Math.floor(mouseX/25)*25
+  yDraw = Math.floor(mouseY/25)*25
+  var cType=board[yDraw/25][xDraw/25].type
+  if(cType=="start"){
+    startPoints-=1
+    updateText()
+  }else if(cType=="end"){
+    endPoints-=1
+    updateText()
+  }
+
+  if(type=="wall"){
+    if(board[yDraw/25][xDraw/25].type!="wall"){
+      board[yDraw/25][xDraw/25].type="wall"
+      ctx.fillStyle="#383838"
+      ctx.fillRect(xDraw,yDraw,25,25);
+      wallCount+=1
+      walls.push(board[yDraw/25][xDraw/25])
+      updateText()
+    }
+
+  }else if (type=="start") {
+    if(startPoints==0){
+      startPoints+=1
+      board[yDraw/25][xDraw/25].type="start"
+      board[yDraw/25][xDraw/25].start=true;
+      startNode=board[yDraw/25][xDraw/25]
+      ctx.fillStyle="#61bf44"
+      ctx.fillRect(xDraw,yDraw,25,25);
+
+      updateText()
+    }
+
+  }else if (type=="end") {
+    if(endPoints==0){
+      endPoints+=1
+      board[yDraw/25][xDraw/25].type="end"
+      board[yDraw/25][xDraw/25].end=true;
+      endNode=board[yDraw/25][xDraw/25]
+
+      ctx.fillStyle="#af4646"
+      ctx.fillRect(xDraw,yDraw,25,25);
+      updateText()
+    }
+
+  }else if (type=="eraser") {
+    if(board[yDraw/25][xDraw/25].type!="empty"){
+      if(board[yDraw/25][xDraw/25].type=="wall"){
+        wallCount-=1
+
+        removeFromArr(walls,board[yDraw/25][xDraw/25])
+        updateText()
+
+      }else if (board[yDraw/25][xDraw/25].type=="end") {
+        endPoints-=1
+        updateText()
+
+      }else{
+        startPoints-=1
+        updateText()
+      }
+
+      board[yDraw/25][xDraw/25].type="empty"
+      ctx.fillStyle="white"
+      ctx.fillRect(xDraw,yDraw,25,25);
+    }
+  }
+
+
+}
+
+function clearAll(){
+  for(i=0;i<59;i++){
+    for(j=0;j<29;j++){
+      board[j][i].type="empty"
+    }
+  }
+  ctx.clearRect(0, 0, 1500, 750)
+  wallCount=0
+  endPoints=0
+  startPoints=0
+
+
+  updateText()
+}
+
+function checkPos(mouseEvent){
+    mouseX = mouseEvent.pageX - this.offsetLeft;
+    mouseY = mouseEvent.pageY - this.offsetTop;
+
+  if(mouseEvent.pageX || mouseEvent.pageY == 0){
+    mouseX = mouseEvent.pageX - this.offsetLeft;
+    mouseY = mouseEvent.pageY - this.offsetTop;
+  }else if(mouseEvent.offsetX || mouseEvent.offsetY == 0){
+    mouseX = mouseEvent.offsetX;
+    mouseY = mouseEvent.offsetY;
+  }
+
+  if (mouseDown==true){
+    draw()
+  }
+
+}
+
+function checkClick(mouseEvent){
+  mouseDown=false
+}
+
+function typeOfBlock(t){
+  type=t
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//stay pls
